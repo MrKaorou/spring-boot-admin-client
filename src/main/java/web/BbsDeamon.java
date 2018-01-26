79f88b19c261c1190bd735da30d1dc2bb42af675
MrKaorou <Mr.kaorou@gmail.com>
2018-01-26 13:52:35 +0800

    默认的

--- src/main/java/web/BbsDeamon.java
+++ src/main/java/web/BbsDeamon.java
@@ -3,15 +3,18 @@ package web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by Administrator on 2017/8/24.
  */
+@SpringBootApplication
 @RestController
 @EnableAutoConfiguration
-public class BbsDeamon {
+public class BbsDeamon extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     public String index() {
