323ce55bb4bd3ed8e3a51fbed9eb4b84264d4cdb
MrKaorou <Mr.kaorou@gmail.com>
2018-03-26 16:59:24 +0800

    23333

--- src/main/java/web/BbsDeamon.java
+++ src/main/java/web/BbsDeamon.java
@@ -1,6 +1,5 @@
 package web;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,7 +8,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Created by Administrator on 2017/8/24.
+ * demo
+ * @author mihao
+ * @date 2018-3-26
+ * @version 1.0
  */
 @SpringBootApplication
 @RestController
@@ -18,7 +20,7 @@ public class BbsDeamon extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     public String index() {
-        return "欢迎光临小春论坛!";
+        return "你好！我是8001";
     }
 
     public static void main(String[] args) {
